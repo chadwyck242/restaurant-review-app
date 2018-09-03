55d7f2c768f4255e29902a3de80ec202a3bf9b90
chadwyck242 <chadwyck.codeart@gmail.com>
2018-09-03 13:56:08 -0600

    Add more ARIA and Ally components

--- sw.js
+++ sw.js
@@ -29,7 +29,17 @@ self.addEventListener('install', event => {
         '/img/8.jpg',
         '/img/9.jpg',
         '/img/10.jpg',
-        '/img/fallback.jpg'
+        '/img/fallback.jpg',
+        '/restaurant.html?id=1',
+        '/restaurant.html?id=2',
+        '/restaurant.html?id=3',
+        '/restaurant.html?id=4',
+        '/restaurant.html?id=5',
+        '/restaurant.html?id=6',
+        '/restaurant.html?id=7',
+        '/restaurant.html?id=8',
+        '/restaurant.html?id=9',
+        '/restaurant.html?id=10'
       ]);
     })
   );
