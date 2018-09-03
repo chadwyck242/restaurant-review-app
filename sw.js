93628533cc504a76cd3a429058d9bfd408ea62b9
chadwyck242 <chadwyck.codeart@gmail.com>
2018-09-03 12:07:35 -0600

    Finish building out the Service Worker

--- sw.js
+++ sw.js
@@ -3,24 +3,71 @@ offline capabilities
 This follows the MDN tutorial @:
 https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
 */
-let cacheVersion = 'fend-rrapp-v01';
+let staticCacheName = 'fend-rrapp-v01';
 
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheVersion).then(cache => {
+    caches.open(staticCacheName).then(cache => {
       return cache.addAll([
         '/',
         '/index.html',
         '/restaurant.html',
         '/css/styles.css',
-        '/data/restaurants.json',
         '/js/',
         '/js/dbhelper.js',
         '/js/main.js',
         '/js/restaurant_info.js',
-        '/js/serviceworker.js'
+        '/data/restaurants.json',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg',
+        '/img/fallback.jpg'
       ]);
     })
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName.startsWith('fend-') &&
+                 cacheName != staticCacheName;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request).then(response => {
+        let responseClone = response.clone();
+        caches.open(staticCacheName).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      });
+    }).catch(function() {
+      return caches.match('/img/fallback.jpg');
+    })
+  );
+});
+
+self.addEventListener('message', event => {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
